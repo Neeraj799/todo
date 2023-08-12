f08086a07553d69c99810ec3e9bb660358089a6f
neeraj <neerajpc799@gmail.com>
2023-08-12 11:28:07 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var inputElement = document.getElementById("input");
-
+var todoList = document.getElementById("to-do-list");
 inputElement.addEventListener("keydown" , function(e){
     if(e.key == "Enter") {
-        console.log(e.target.value)
+        if(e.target.value == ""){
+            alert("Please enter the item name");
+        }else {
+        var listItem = document.createElement("li");
+        listItem.innerText = e.target.value;
+        todoList.appendChild(listItem);
+        e.target.value = ""
+
+        }
     }
 
 })
