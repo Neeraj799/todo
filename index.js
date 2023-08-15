ccb8cbde5809e2ba2721294c6a8e1b8fc93c4578
neeraj <neerajpc799@gmail.com>
2023-08-15 19:34:50 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var inputElement = document.getElementById("input");
 var todoList = document.getElementById("to-do-list");
+// Add event listener to the button element, and call addItem() when clicked
 inputElement.addEventListener("keydown" , function(e){
     if(e.key == "Enter") {
         if(e.target.value == ""){
@@ -7,7 +8,17 @@ inputElement.addEventListener("keydown" , function(e){
         }else {
         var listItem = document.createElement("li");
         listItem.innerText = e.target.value;
+        //Add delete button to each element in the list
+        var deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "delete item";
+      
+        deleteBtn.addEventListener("click" , function(){
+            listItem.remove();
+        })
+
+        listItem.appendChild(deleteBtn);
         todoList.appendChild(listItem);
+
         e.target.value = ""
 
         }
